403f65da3ff2193befe3a2f70e574c2860a90948
Jonathas Tavares <jonathasl.tavares@gmail.com>
2022-11-10 21:21:31 -0300

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { LoginComponent } from './pages/login/login.component';
+import { LoginFormComponent } from './pages/login/components/login-form/login-form.component';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    LoginComponent
+    LoginComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
