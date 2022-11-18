6a7b6424beda5dedcbe7104b9f0bf7102c1ba27f
Jonathas Tavares <jonathasl.tavares@gmail.com>
2022-11-18 17:17:09 -0300

    modal delete

--- src/app/pages/home/components/produtos/ver-mais-prod/ver-mais-prod.component.ts
+++ src/app/pages/home/components/produtos/ver-mais-prod/ver-mais-prod.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ProductsService } from 'src/app/services/products.service';
 import { Produtos } from 'src/app/shared/models/produtos';
+declare var window: any;
 
 @Component({
   selector: 'app-ver-mais-prod',
@@ -10,9 +11,10 @@ import { Produtos } from 'src/app/shared/models/produtos';
 })
 export class VerMaisProdComponent {
   public produto!:Produtos
+  formModal: any;
   constructor(private produtoService: ProductsService,private activatedRoute : ActivatedRoute, private route:Router){
-    
   }
+
   ngOnInit(): void {
     let id = this.activatedRoute.snapshot.paramMap.get("id");
     if(typeof id === 'string'){
@@ -21,6 +23,11 @@ export class VerMaisProdComponent {
       this.produto = result
     })
     }
+
+    this.formModal = new window.bootstrap.Modal(
+      document.getElementById('DeleteModal')
+    );
+  
   }
 
   deletarProduto(id:string){
@@ -29,10 +36,16 @@ export class VerMaisProdComponent {
 
       }, erro =>{
         if(erro.status == 200){
+          this.formModal.hide();
           window.alert("Produto deletado")
           this.route.navigate(['/home/produtos'])
         }
       }
     )
   }
+
+  openFormModal() {
+    this.formModal.show();
+  }
+  
 }
